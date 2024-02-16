834e5a75e5f4e1be8123a7021927def8a1d4e29f
Onur Gonullu <21060336@stu.omu.edu.tr>
2024-02-16 16:15:46 +0300

    init

---
+++ src/main/java/com/app/facebookclone/FacebookcloneApplication.java
@@ -0,0 +1,13 @@
+package com.app.facebookclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/facebookclone/FacebookcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.facebookclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
